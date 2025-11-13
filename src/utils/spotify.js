e23c2800a788ce92018221df86ccae0ee9fb7940
wilker436 <wikerpachecoperez@gmail.com>
2025-11-13 17:06:29 -0500

    integracion basica de la api

---
+++ src/utils/spotify.js
@@ -0,0 +1,8 @@
+export const SPOTIFY_CONFIG = {
+  clientId: '085367c31dee459bafba7b2c3400df48',
+  clientSecret: 'c8c1d36004484bda8634a5d582805bc6',
+  redirectUri: 'http://127.0.0.1:4321/callback',
+  authUrl: 'https://accounts.spotify.com/authorize',
+  tokenUrl: 'https://accounts.spotify.com/api/token',
+  scope: 'user-library-read playlist-read-private user-read-private'
+};
